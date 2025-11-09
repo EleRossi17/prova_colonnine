0288115d20931329685e3c4de42bb17998a1e293
Elena <115565418+EleRossi17@users.noreply.github.com>
2025-11-09 19:21:05 +0100

    Update papaparse.d.ts

--- app/types/papaparse.d.ts
+++ app/types/papaparse.d.ts
@@ -1,5 +1,4 @@
-// 👇 Questo dice a TypeScript di accettare il modulo papaparse anche senza tipi
-declare module "papaparse" {
+declare module 'papaparse' {
   const Papa: any;
   export default Papa;
 }
